3026ccaf9ec01abf1485eaf1c8e9a03854762fd7
Nam Pho <nampho@me.com>
2019-02-06 01:05:55 -0800

    finished preprocessing scripts for both data sets

---
+++ preprocessing-death.R
@@ -0,0 +1,188 @@
+# preprocessing.R
+# loads linked infant death data from 2012 from NVSS of NCHS
+# https://www.nber.org/data/lbid.html
+# preprocesses for outcome variable and features of interest 
+
+library(data.table)
+library(tidyverse)
+
+###
+# load data
+ddat2012 <- fread("~/Downloads/linkco2012us_den.csv", integer64="character")
+ddat2012 <- Filter(function(x) { n_distinct(x) > 1 }, ddat2012) # removes features with no distinct value
+ddat2012 <- ddat2012 %>% select(-starts_with("f_")) # removes flag columns
+
+###
+# subset and clean data for use
+N <- 250
+ddat.features <- data.frame(x=rep("", N), class=rep("", N), desc=rep("", N), stringsAsFactors=F)
+
+# Temporal Data
+ddat.features[ 1,] <- list("DOB_MM", "factor", "Birth Month")
+ddat.features[ 2,] <- list("DOB_WK", "factor", "Birth Day of Week")
+ddat.features[ 3,] <- list("BFACIL", "factor", "Birth Place")
+
+# Maternal Demographic
+ddat.features[ 4,] <- list("MAGER41", "numeric", "Mother's Single Years of Age")
+ddat.features[ 5,] <- list("RESTATUS", "factor", "Residence Status")
+ddat.features[ 6,] <- list("MEDUC", "factor", "Mother's Education")
+ddat.features[ 7,] <- list("MRACEHISP", "factor", "Mother's Race/Hispanic Origin")
+
+# Paternal Demographic
+ddat.features[ 8,] <- list("FAGECOMB", "numeric", "Father's Combined Age")
+ddat.features[ 9,] <- list("FRACEHISP", "factor", "Father's Race/Hispanic Origin")
+
+# Maternal History
+ddat.features[10,] <- list("LBO", "numeric", "Live Birth Order Recode")
+ddat.features[11,] <- list("TBO", "numeric", "Total Birth Order Recode")
+
+# Pre-delivery Maternal Care
+ddat.features[12,] <- list("PRECARE", "numeric", "Month Prenatal Care Began")
+ddat.features[13,] <- list("UPREVIS", "numeric", "Number of Prenatal Visits")
+ddat.features[14,] <- list("WTGAIN", "numeric", "Weight Gain")
+
+# Fetal Nutrition and Environment
+ddat.features[15,] <- list("CIG_1", "numeric", "Cigarettes 1st Trimester")
+ddat.features[16,] <- list("CIG_2", "numeric", "Cigarettes 2nd Trimester")
+ddat.features[17,] <- list("CIG_3", "numeric", "Cigarettes 3rd Trimester")
+
+# Risk Factors
+ddat.features[18,] <- list("RF_DIAB", "factor", "Pre-pregnancy Diabetes")
+ddat.features[19,] <- list("RF_GEST", "factor", "Gestational Diabetes")
+ddat.features[20,] <- list("RF_PHYP", "factor", "Pre-pregnancy Hypertension")
+ddat.features[21,] <- list("RF_GHYP", "factor", "Gestational Hypertension")
+ddat.features[22,] <- list("RF_ECLAM", "factor", "Hypertension Eclampsia")
+ddat.features[23,] <- list("RF_PPTERM", "factor", "Previous Preterm Birth")
+ddat.features[24,] <- list("RF_PPOUTC", "factor", "Poor Pregnancy Outcome")
+ddat.features[25,] <- list("RF_CESAR", "factor", "Previous Cesareans")
+ddat.features[26,] <- list("RF_NCESAR", "numeric", "Number of Previous Cesareans")
+ddat.features[27,] <- list("URF_DIAB", "factor", "Diabetes")
+ddat.features[28,] <- list("URF_CHYPER", "factor", "Chronic Hypertension")
+ddat.features[29,] <- list("URF_PHYPER", "factor", "Pre-pregnancy Associated Hypertension")
+ddat.features[30,] <- list("URF_ECLAM", "factor", "Eclampsia")
+
+# Obstetric Procedures
+ddat.features[31,] <- list("OP_CERV", "factor", "Cervical Cerclage")
+ddat.features[32,] <- list("OP_TOCOL", "factor", "Tocolysis")
+ddat.features[33,] <- list("OP_ECVS", "factor", "Successful External Cephalic Version")
+ddat.features[34,] <- list("OP_ECVF", "factor", "Failed External Cephalic Version")
+ddat.features[35,] <- list("UOP_INDUC", "factor", "Induction of Labor")
+ddat.features[36,] <- list("UOP_TOCOL", "factor", "Tocolysis")
+
+# Onset of Labor
+ddat.features[37,] <- list("ON_RUPTR", "factor", "Pre-mature Rupture of Membrane")
+ddat.features[38,] <- list("ON_PROLG", "factor", "Prolonged Labor")
+
+# Characteristics of Labor and Delivery
+ddat.features[39,] <- list("LD_INDUCT", "factor", "Induction of Labor")
+ddat.features[40,] <- list("ld_augment", "factor", "Augmentation of Labor")
+ddat.features[41,] <- list("ld_steroids", "factor", "Steroids")
+ddat.features[42,] <- list("ld_antibio", "factor", "Antibiotics")
+ddat.features[43,] <- list("ld_chorio", "factor", "Chorioamnionitis")
+ddat.features[44,] <- list("ld_mecon", "factor", "Meconium Staining")
+ddat.features[45,] <- list("ld_fintol", "factor", "Fetal Intolerance")
+ddat.features[46,] <- list("ld_anesth", "factor", "Anesthesia")
+
+# Labor Delivery Complications
+ddat.features[47,] <- list("uld_meco", "factor", "Meconium")
+ddat.features[48,] <- list("uld_precip", "factor", "Precipitous Labor")
+ddat.features[49,] <- list("uld_breech", "factor", "Breech")
+
+ddat.features[50,] <- list("md_present", "factor", "Fetal Presentation")
+ddat.features[51,] <- list("md_route", "factor", "Final Route and Method of Delivery")
+ddat.features[52,] <- list("md_trial", "factor", "Trial of Labor Attempted")
+
+# Method of Delivery
+ddat.features[53,] <- list("ume_forcp", "factor", "Forceps")
+ddat.features[54,] <- list("ume_vac", "factor", "Vacuum")
+ddat.features[55,] <- list("rdmeth_rec", "factor", "Delivery Method Recode")
+
+ddat.features[56,] <- list("ATTEND", "factor", "Attendant at Birth")
+
+ddat.features[57,] <- list("apgar5", "numeric", "Five Minute APGAR Score")
+ddat.features[58,] <- list("DPLURAL", "numeric", "Birth Plurality")
+ddat.features[59,] <- list("SEX", "factor", "Sex of Baby")
+ddat.features[60,] <- list("dlmp_mm", "factor", "Last Normal Menses Month")
+ddat.features[61,] <- list("dlmp_yy", "numeric", "Year of Last Normal Menses Began")
+ddat.features[62,] <- list("estgest", "numeric", "Obstetric/Clinical Gestation Est")
+ddat.features[63,] <- list("dbwt", "numeric", "Delivery Birth Weight in Grams")
+
+# Abnormal Conditions of the Newborn 517-536
+ddat.features[64,] <- list("ab_vent", "factor", "Assisted Ventilation")
+ddat.features[65,] <- list("ab_vent6", "factor", "Assisted Ventilation > 6 hrs")
+ddat.features[66,] <- list("ab_nicu", "factor", "Admission to NICU")
+ddat.features[67,] <- list("ab_surfac", "factor", "Surfactant")
+ddat.features[68,] <- list("ab_antibio", "factor", "Antibiotics")
+
+# Congenital Anomalies of the Newborn 537-566
+ddat.features[69,] <- list("CA_ANEN", "factor", "Anencephaly")
+ddat.features[70,] <- list("ca_menin", "factor", "Meningomyelocele / Spina Bifida")
+ddat.features[71,] <- list("ca_heart", "factor", "Cyanotic Congenital Heart Disease")
+ddat.features[72,] <- list("ca_hernia", "factor", "Congenital Diaphragmatic Hernia")
+ddat.features[73,] <- list("ca_ompha", "factor", "Omphalocele")
+ddat.features[74,] <- list("ca_gastro", "factor", "Gastroschisis")
+ddat.features[75,] <- list("CA_LIMB", "factor", "Limb Reduction Defect")
+ddat.features[76,] <- list("ca_cleftlp", "factor", "Cleft Lip w/ or w/o Cleft Palate")
+ddat.features[77,] <- list("ca_cleft", "factor", "Cleft Palate alone")
+ddat.features[78,] <- list("CA_DOWNS", "factor", "Down Syndrome")
+ddat.features[79,] <- list("ca_chrom", "factor", "Suspected Chromosomal Disorder")
+ddat.features[80,] <- list("ca_hypos", "factor", "Hypospadias")
+
+ddat.features[81,] <- list("uca_anen", "factor", "Anencephalus")
+ddat.features[82,] <- list("uca_spina", "factor", "Spina Bifida")
+ddat.features[83,] <- list("uca_ompha", "factor", "Omphalocele / Gastroschisis")
+ddat.features[84,] <- list("uca_cleftlp", "factor", "Cleft Lip/Palate ")
+ddat.features[85,] <- list("uca_downs", "factor", "Downs Syndrome")
+
+ddat.features[86,] <- list("aged", "numeric", "Age at Death in Days")
+ddat.features[87,] <- list("place", "factor", "Place of injury")
+
+ddat.features <- ddat.features[ddat.features$x!="", ] # drop empty rows
+ddat.features$x <- ddat.features$x %>% tolower()
+
+# just get the features and correct mutated columns
+ddat <- ddat2012 %>% select(ddat.features$x) %>% as.data.frame()
+
+# collapse into outcome variable, alive after 1 year? yes (1) or no (0)
+ddat$alive <- ddat$aged %>% replace_na(-1)
+ddat$alive <- ifelse(ddat$alive > -1, 0, -1)
+ddat$alive <- -1 * ddat$alive
+ddat$alive <- as.factor(ddat$alive)
+
+# remove unknown values, either 'U' for categorical or the max value for numerics
+mask.factor  <- colnames(ddat) %in% subset(ddat.features, class == "factor")$x
+ddat[, mask.factor]  <- lapply(ddat[, mask.factor],  function(x) { na_if(x, 'U') %>% as.factor() })
+
+mask.numeric <- colnames(ddat) %in% subset(ddat.features, class == "numeric")$x
+ddat[, mask.numeric] <- lapply(ddat[, mask.numeric], function(x) { na_if(x, max(unique(x))) %>% as.numeric() })
+
+#sapply(ddat, class) # sanity check classes of features
+
+###
+# split data into training and testing subsets
+
+# table(ddat$alive) %>% prop.table() # complications to mom or child occur in 0.59% 23401/(23401+3937396)
+
+# split all the data
+
+# sub-sample for smaller data sets, faster iterations (80-20 for test-train)
+i <- base::sample(1:nrow(ddat), replace=FALSE, size=1e4) # 10,000
+ddat.test  <- ddat %>% slice(i) 
+
+tmp <- ddat %>% slice(-i) %>% filter(alive==1) # YES alive
+j <- base::sample(1:nrow(tmp), replace=FALSE, size=2e4) # 20,000
+ddat.train <- tmp %>% slice(j)
+
+tmp <- ddat %>% slice(-i) %>% filter(alive==0) # NO alive
+j <- base::sample(1:nrow(tmp), replace=FALSE, size=2e4) # 20,000
+ddat.train <- tmp %>% slice(j)
+
+# sanity check 
+#data_test %>% apply(2, function(x) { prop.table(table(x)) })
+#data_test %>% apply(2, function(x) { prop.table(table(x)) })
+
+###
+# data ready to be processed by different supervised learning algorithms
+cat("Writing processed death data...\n")
+save(ddat, file=paste0("~/src/family-health/death-all.RData"))
+save(ddat.test, ddat.train, ddat.features, file=paste0("~/src/family-health/death-", as.numeric(as.POSIXct(Sys.time())), ".RData"))
---
+++ preprocessing-natality.R
@@ -0,0 +1,184 @@
+# preprocessing-natality.R
+# loads natality data from 2017 from NVSS of NCHS
+# https://www.nber.org/data/vital-statistics-natality-data.html 
+# preprocesses for outcome variable and features of interest 
+
+library(data.table)
+library(tidyverse)
+
+###
+# load data
+natl2017 <- fread("~/Downloads/natl2017.csv", integer64="character")
+natl2017 <- Filter(function(x) { n_distinct(x) > 1 }, natl2017) # removes features with no distinct value
+
+###
+# subset and clean data for use
+N <- 73
+nicu.features <- data.frame(x=rep("", N), class=rep("", N), desc=rep("", N), stringsAsFactors=F)
+
+# Temporal Data
+nicu.features[ 1,] <- list("DOB_MM", "factor", "Birth Month")
+nicu.features[ 2,] <- list("DOB_WK", "factor", "Birth Day of Week")
+nicu.features[ 3,] <- list("BFACIL", "factor", "Birth Place")
+
+# Maternal Demographic
+nicu.features[ 4,] <- list("MAGER", "numeric", "Mother's Single Years of Age")
+nicu.features[ 5,] <- list("MRACEHISP", "factor", "Mother's Race/Hispanic Origin")
+nicu.features[ 6,] <- list("DMAR", "factor", "Marital Status")
+nicu.features[ 7,] <- list("MEDUC", "factor", "Mother's Education")
+
+# Paternal Demographic
+nicu.features[ 8,] <- list("FAGECOMB", "numeric", "Father's Combined Age")
+nicu.features[ 9,] <- list("FRACEHISP", "factor", "Father's Race/Hispanic Origin")
+nicu.features[10,] <- list("FEDUC", "factor", "Father's Education")
+
+# Maternal History
+nicu.features[11,] <- list("PRIORLIVE", "numeric", "Prior Births Now Living")
+nicu.features[12,] <- list("PRIORDEAD", "numeric", "Prior Births Now Dead")
+nicu.features[13,] <- list("PRIORTERM", "numeric", "Prior Other Terminations")
+nicu.features[14,] <- list("LBO_REC", "numeric", "Live Birth Order Recode")
+nicu.features[15,] <- list("TBO_REC", "numeric", "Total Birth Order Recode")
+nicu.features[16,] <- list("ILLB_R", "numeric", "Interval Since Last Live Birth Recode")
+nicu.features[17,] <- list("ILOP_R", "numeric", "Interval Since Last Other Pregnancy Recode")
+
+# Pre-delivery Maternal Care
+nicu.features[18,] <- list("PRECARE", "numeric", "Month Prenatal Care Began")
+nicu.features[19,] <- list("PREVIS", "numeric", "Number of Prenatal Visits")
+
+# Fetal Nutrition and Environment
+nicu.features[20,] <- list("WIC", "factor", "Special Supplemental Nutrition Program for Women, Infants, and Children")
+nicu.features[21,] <- list("CIG_0", "numeric", "Cigarettes Before Pregnancy")
+nicu.features[22,] <- list("CIG_1", "numeric", "Cigarettes 1st Trimester")
+nicu.features[22,] <- list("CIG_2", "numeric", "Cigarettes 2nd Trimester")
+nicu.features[23,] <- list("CIG_3", "numeric", "Cigarettes 3rd Trimester")
+
+# Maternal Features
+nicu.features[24,] <- list("M_Ht_In", "numeric", "Mother's Height in Total Inches")
+nicu.features[25,] <- list("BMI", "numeric", "Mother's BMI")
+nicu.features[26,] <- list("PWgt_R", "numeric", "Pre-pregnancy Weight Recode")
+#nicu.features[27,] <- list("DBWT", "numeric", "Delivery Weight")
+nicu.features[28,] <- list("WTGAIN", "numeric", "Weight Gain")
+
+# Risk Factors 313-342
+nicu.features[29,] <- list("RF_PDIAB", "factor", "Pre-pregnancy Diabetes")
+nicu.features[30,] <- list("RF_GDIAB", "factor", "Gestational Diabetes")
+nicu.features[31,] <- list("RF_PHYPE", "factor", "Pre-pregnancy Hypertension")
+nicu.features[32,] <- list("RF_GHYPE", "factor", "Gestational Hypertension")
+nicu.features[33,] <- list("RF_EHYPE", "factor", "Hypertension Eclampsia")
+nicu.features[34,] <- list("RF_PPTERM", "factor", "Previous Preterm Birth")
+nicu.features[35,] <- list("RF_INFTR", "factor", "Infertility Treatment Used")
+nicu.features[36,] <- list("RF_FEDRG", "factor", "Fertility Enhancing Drugs")
+nicu.features[37,] <- list("RF_ARTEC", "factor", "Asst. Reproductive Technology")
+nicu.features[38,] <- list("RF_CESAR", "factor", "Prevoius Cesarean")
+nicu.features[39,] <- list("RF_CESARN", "numeric", "Number of Previous Cesareans")
+#nicu.features[40,] <- list("NO_RISKS", "factor", "No Risk Factors Reported")
+
+# Infections Present 343-358 
+nicu.features[41,] <- list("IP_GON", "factor", "Gonorrhea")
+nicu.features[42,] <- list("IP_SYPH", "factor", "Syphilis")
+nicu.features[43,] <- list("IP_CHLAM", "factor", "Chlamydia")
+nicu.features[44,] <- list("IP_HEPatB", "factor", "Hepatitis B")
+nicu.features[45,] <- list("IP_HEPatC", "factor", "Hepatitis C")
+#nicu.features[46,] <- list("NO_INFEC", "factor", "No Infections Reported")
+
+# Obstetric Procedures 359-370
+nicu.features[47,] <- list("OB_ECVS", "factor" , "Successful External Cephalic Version")
+nicu.features[48,] <- list("OB_ECVF", "factor", "Failed External Cephalic Version")
+
+# Characteristics of Labor and Delivery 383-400
+nicu.features[49,] <- list("LD_INDL", "factor", "Induction of Labor")
+nicu.features[50,] <- list("LD_AUGM", "factor", "Augmentation of Labor")
+nicu.features[51,] <- list("LD_STER", "factor", "Steroids")
+nicu.features[52,] <- list("LD_ANTB", "factor", "Antibiotics")
+nicu.features[53,] <- list("LD_CHOR", "factor", "Chorioamnionitis")
+nicu.features[54,] <- list("LD_ANES", "factor", "Anesthesia")
+
+# Maternal Morbidity 415-432 
+# NO_MMORB, No Maternal Morbidity, outcome variable
+
+# Administrative
+nicu.features[55,] <- list("ATTEND", "factor", "Attendant at Birth")
+nicu.features[56,] <- list("PAY", "factor", "Payment Source for Delivery")
+
+# Other Baby Info
+# Skip APGAR
+nicu.features[57,] <- list("DPLURAL", "numeric", "Birth Plurality")
+nicu.features[58,] <- list("SETORDER_R", "numeric", "Set Order Recode")
+nicu.features[59,] <- list("SEX", "factor", "Sex of Baby")
+#nicu.features[60,] <- list("COMBGEST", "numeric", "Combined Gestation")
+
+# Abnormal Conditions of the Newborn 517-536
+# NO_ABNORM, No Abnormal Conditions Checked, outcome variable
+
+# Congenital Anomalies of the Newborn 537-566
+nicu.features[61,] <- list("CA_ANEN", "factor", "Anencephaly")
+nicu.features[62,] <- list("CA_MNSB", "factor", "Meningomyelocele / Spina Bifida")
+nicu.features[63,] <- list("CA_CCHD", "factor", "Cyanotic Congenital Heart Disease")
+nicu.features[64,] <- list("CA_CDH", "factor", "Congenital Diaphragmatic Hernia")
+nicu.features[65,] <- list("CA_OMPH", "factor", "Omphalocele")
+nicu.features[66,] <- list("CA_GAST", "factor", "Gastroschisis")
+nicu.features[67,] <- list("CA_LIMB", "factor", "Limb Reduction Defect")
+nicu.features[68,] <- list("CA_CLEFT", "factor", "Cleft Lip w/ or w/o Cleft Palate")
+nicu.features[69,] <- list("CA_CLPAL", "factor", "Cleft Palate alone")
+nicu.features[70,] <- list("CA_DOWNs", "factor", "Down Syndrome")
+nicu.features[71,] <- list("CA_DISOR", "factor", "Suspected Chromosomal Disorder")
+nicu.features[72,] <- list("CA_HYPO", "factor", "Hypospadias")
+#nicu.features[73,] <- list("NO_CONGEN", "factor", "No Congenital Anomalies Checked")
+
+nicu.features <- nicu.features[nicu.features$x!="", ] # drop empty rows
+nicu.features$x <- nicu.features$x %>% tolower()
+
+# collapse all maternal complications into yes (1) or no (0)
+natl2017 <- natl2017 %>% 
+            mutate(m_comp=ifelse(mm_mtr=='Y'|mm_plac=='Y'|mm_rupt=='Y'|mm_uhyst=='Y'|mm_aicu=='Y', 1, 0))
+
+# collapse all baby complications into yes (1) or no (0)
+natl2017 <- natl2017 %>% 
+            mutate(b_comp=ifelse(ab_aven1=='Y'|ab_aven6=='Y'|ab_nicu=='Y'|ab_surf=='Y'|ab_anti=='Y'|ab_seiz=='Y', 1, 0))
+
+# collapse either maternal or baby complications into yes (1) or no (0)
+natl2017 <- natl2017 %>% mutate(e_comp=ifelse(m_comp==1|b_comp==1, 1, 0))
+
+# just get the features and correct mutated columns
+nicu <- natl2017 %>% select(m_comp, b_comp, e_comp, nicu.features$x) %>% as.data.frame()
+nicu$m_comp <- as.factor(nicu$m_comp)
+nicu$b_comp <- as.factor(nicu$b_comp)
+nicu$e_comp <- as.factor(nicu$e_comp)
+
+# remove unknown values, either 'U' for categorical or the max value for numerics
+mask.factor  <- colnames(nicu) %in% subset(nicu.features, class == "factor" )$x
+nicu[, mask.factor]  <- lapply(nicu[, mask.factor],  function(x) { na_if(x, 'U') %>% as.factor() })
+
+mask.numeric <- colnames(nicu) %in% subset(nicu.features, class == "numeric")$x
+nicu[, mask.numeric] <- lapply(nicu[, mask.numeric], function(x) { na_if(x, max(unique(x))) %>% as.numeric() })
+
+#sapply(nicu, class) # sanity check classes of features
+
+###
+# split data into training and testing subsets
+
+# table(nicu$e_comp) %>% prop.table() # complications to mom or child occur in 11.9% 459221/(459221+3405533)
+
+# sub-sample for smaller data sets, faster iterations (80-20 for test-train)
+i <- base::sample(1:nrow(nicu), replace=FALSE, size=1e4) # 10,000
+nicu.test  <- nicu %>% slice(i) 
+
+tmp <- nicu %>% slice(-i) %>% filter(e_comp==1) # YES complications
+j <- base::sample(1:nrow(tmp), replace=FALSE, size=2e4) # 20,000
+nicu.train1 <- tmp %>% slice(j)
+
+tmp <- nicu %>% slice(-i) %>% filter(e_comp==0) # NO complications
+j <- base::sample(1:nrow(tmp), replace=FALSE, size=2e4) # 20,000
+nicu.train2 <- tmp %>% slice(j)
+
+nicu.train <- rbind(nicu.train1, nicu.train2)
+
+# sanity check 
+#data_test %>% apply(2, function(x) { prop.table(table(x)) })
+#data_test %>% apply(2, function(x) { prop.table(table(x)) })
+
+###
+# data ready to be processed by different supervised learning algorithms
+cat("Writing processed natality data...\n")
+save(nicu, file=paste0("~/src/family-health/nicu-all.RData"))
+save(nicu.test, nicu.train, nicu.features, file=paste0("~/src/family-health/nicu-", as.numeric(as.POSIXct(Sys.time())), ".RData"))
