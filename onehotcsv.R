71661a58d2692a0bcf7cf0174dc521bd07cb69d9
Nam Pho <nampho@me.com>
2019-03-02 22:42:30 -0800

    adding feature scaling for numerics in addition to one hot encoding

--- onehotcsv.R
+++ onehotcsv.R
@@ -11,10 +11,20 @@ nicu.test <- nicu.test %>% select(e_comp, nicu.features$x)
 
 set.seed(1)
 
-dummy.train <- dummyVars(~., data=nicu.train, fullRank=T)
-nicu.train.hot <- data.frame(predict(dummy.train, newdata=nicu.train))
+# scale training data
+scale.train <- preProcess(nicu.train, method=c("center", "scale"))
+scale.train <- data.frame(predict(scale.train, newdata=nicu.train))
+
+# one hot encode training data
+dummy.train <- dummyVars(~., data=scale.train, fullRank=T)
+nicu.train.hot <- data.frame(predict(dummy.train, newdata=scale.train))
 write.csv(nicu.train.hot, "dat/nicu-train-hot.csv", quote=F, row.names=F)
 
-dummy.test  <- dummyVars(~., data=nicu.test, fullRank=T)
-nicu.test.hot  <- data.frame(predict(dummy.test,  newdata=nicu.test))
+# scale testing data
+scale.test <- preProcess(nicu.test, method=c("center", "scale"))
+scale.test <- data.frame(predict(scale.test, newdata=nicu.test))
+
+# one hot encode testing data
+dummy.test  <- dummyVars(~., data=scale.test, fullRank=T)
+nicu.test.hot  <- data.frame(predict(dummy.test,  newdata=scale.test))
 write.csv(nicu.test.hot,  "dat/nicu-test-hot.csv",  quote=F, row.names=F)
